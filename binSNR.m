f819408c5acad7f923a4b5ac12488ef71ea89448
BarathMohanU <76472410+BarathMohanU@users.noreply.github.com>
2021-01-29 16:19:19 +0530

    Smoler

--- binSNR.m
+++ binSNR.m
@@ -2,14 +2,14 @@ function SNR = binSNR(sig)
 
     L = length(sig);
     Fs = 8000;
-    f = Fs*(0:(L))/L;
+    f = Fs*(0:(L-1))/L;
 
     power = fft(sig).*conj(fft(sig));
 
     Enoise = 0;
     Esig = 0;
 
-    for i=1:length(f)-1
+    for i=1:length(f)
         if mod(f(i),600) == 0
             Enoise = Enoise + power(i);
         else
